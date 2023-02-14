f32acea662c274447b35df2e9ce054f348056b0f
markischengg <markischeng28@uchicago.edu>
2023-02-14 20:04:48 +0000

    users table and details for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,17 @@
 class UsersController < ApplicationController
   def index
-    
+    matching_users = User.all
+    @list_of_users = matching_users.order({ :username => :asc})
     render({ :template => "users_template/index.html.erb"})
+  end
+
+  def show
+    url_username = params.fetch("username")
+
+   matching_usernames = User.where({:username => url_username})
+
+   @the_user = matching_usernames.first
+
+   render({ :template => "users_template/show.html.erb"})
   end
 end
