d88c98584d7139d51e1b4d8c896ad09ac05311cf
markischengg <markischeng28@uchicago.edu>
2023-02-15 04:30:52 +0000

    delete photos

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   get("/users", { :controller => "users", :action => "index"})
 
   get("/users/:username", { :controller => "users", :action => "show"})
-  
+
   get("/photos", { :controller => "photos", :action => "index"})
+
+  get("/photos/:path_id", { :controller => "photos", :action => "show"})
+
+  get("/delete_photo/:path_id", { :controller => "photos", :action => "delete"})
 end
   
